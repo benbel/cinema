9ed3befb40e56c74b93c233a48e0a3aa74c47446
Benjamin Belrhomari <b@belrhomari.fr>
2023-06-07 17:06:11 +0200

    Update build_page.py

--- programs/build_page.py
+++ programs/build_page.py
@@ -112,11 +112,11 @@ def generate_html_film(film, results):
 <details>
 <summary>{film}<br><small>{seances}</small></summary>
 <div class="container">
-<div class="image"><img src=\"{film}.jpg\" width=\"160\"></div>
+<div class="image"><img src=\"{film_path}.jpg\" width=\"160\"></div>
 <div class="text"><small>{jour_sortie}</small> <br> {synopsis}</div>
 <br>
 </details>
-""".format(film = normalise_path(film), jour_sortie = jour_sortie, seances = seances, synopsis = synopsis)
+""".format(film = film, film_path = normalise_path(film), jour_sortie = jour_sortie, seances = seances, synopsis = synopsis)
 
     return html_chunk
 
